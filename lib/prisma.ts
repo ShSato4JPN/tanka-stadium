dd5106e36faa37a24a5866841a0f4b8e22c30455
ShSato4JPN <shsato4.private.1221@gmail.com>
2024-03-05 04:32:29 +0900

    feat: add prisma and sample schema

---
+++ lib/prisma.ts
@@ -0,0 +1,19 @@
+import { PrismaClient } from "@prisma/client";
+
+// global.prisma に明示的に型を定義
+declare global {
+  var prisma: PrismaClient;
+}
+
+let prisma: PrismaClient;
+
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient();
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+  }
+  prisma = global.prisma;
+}
+
+export default prisma;
