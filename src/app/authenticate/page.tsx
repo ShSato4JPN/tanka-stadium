f37e4ee8ccd977dc2c82a0cc2f5fe1f643b623dd
ShSato4JPN <shsato4.private.1221@gmail.com>
2024-03-09 17:35:28 +0900

    fix: add react suspense

--- src/app/authenticate/page.tsx
+++ src/app/authenticate/page.tsx
@@ -1,5 +1,11 @@
-import Authenticate from "@/components/Authenticate";
+import { Suspense } from "react";
 
-export default function AuthenticatePage() {
-  return <Authenticate />;
+import Authenticate from "@/components/Authenticate";
+export default function Page() {
+  return (
+    // TODO　fallback は後で実装
+    <Suspense>
+      <Authenticate />
+    </Suspense>
+  );
 }
